9e76061d7475998d3949c8b0fff601d450588d1b
SidickSino <sidickabdoulayesino1@gmail.com>
2026-02-11 12:16:51 +0200

    Map API URLs to the root path.

--- Fat/urls.py
+++ Fat/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('API.urls')),
+    path('', include('API.urls')),
     path('myapp/', include('Myapp.urls')),
 ]
